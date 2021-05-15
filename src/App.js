bdeeb583b45e69b53ac1f979f3bbd09720d8b768
Michael Jaggers <michael.jaggers@gmail.com>
2021-05-15 11:01:54 -0500

    finish adding header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, {Component} from 'react';
-import ToDoBanner from './components/ToDoBanner';
+import WishListBanner from './components/WishListBanner';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Michael',
+      wishes: [
+        {
+          WishListID: 1,
+          ItemName: 'Lawn Mower',
+          Owned: false
+        },
+        {
+          WishListID: 2,
+          ItemName: 'VR Headset',
+          Owned: false
+        }
+      ],
+      showOwned: true
+    }
+  }
 
+  render = () =>
+    <div>
+      <WishListBanner name={this.state.userName} wishes={this.state.wishes} />
+    </div>
 }
 
 export default App;
--- src/components/WishListBanner.js
+++ src/components/WishListBanner.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class WishListBanner extends Component {
   render = () =>
       <h4 className = "bg-primary text-white text-center p-2" >
-      {this.props.name}'s To-Do List
-      ({this.props.wishes.filter(w => !w.Owned).length} items owned)
+      {this.props.name}'s Wish List
+      ({this.props.wishes.filter(w => !w.Owned).length} items)
     </h4>
 }
 
