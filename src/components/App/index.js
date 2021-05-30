0d6ece67b1c127a78f22df279318b4d1977b24dc
michaeljaggers <michael.jaggers@gmail.com>
2021-05-30 17:02:16 -0500

    added routes, first navigation component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-//import Axios from 'axios';
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
 import WishListBanner from '../WishListBanner';
 import WishListCreator from '../WishListCreator';
 import WishItemRow from '../WishItemRow';
 import VisibilityControl from '../VisibilityControl';
+
+import * as ROUTES from '../../constants/routes';
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -91,8 +102,20 @@ export default class App extends Component {
   }
 
   render = () =>
-    <div>
+    <Router>
       <WishListBanner name={this.state.userName} wishes={this.state.wishes} />
+      <div>
+        <Navigation />
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route path={ROUTES.ADMIN} component={AdminPage} />
+      </div>
       <div className="container-fluid">
         <WishListCreator callback={this.createNewWish} />
         <table className="table table-striped table-bordered">
@@ -129,5 +152,5 @@ export default class App extends Component {
           </table>
         }
       </div>
-    </div>
+    </Router>
 }
